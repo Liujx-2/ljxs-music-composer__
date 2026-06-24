eecdf8e9a7394b4cc96ee26673d1e0d0cae5e1b9
Liujx-2 <Liujx_2@163.com>
2026-06-24 15:23:42 +0800

    update 0.3.1
    fix the problem of once-playing
    optimize the program code

--- main.py
+++ main.py
@@ -7,21 +7,18 @@ from decoder import decode
 import music
 import time
 
-f_index = [196, 262, 294, 330, 349, 370, 392, 440, 494, 523, 587, 659, 698, 784, 880]
+f_index = [196, 262, 294, 330, 349, 370, 392, 440, 494, 523, 587, 659, 698, 784, 880, 0]
 codein = ''
 decoded = []
-bpm = None
-pb=0
 times = 0
 ct = time.time()
 mode = 0
 mode_note = ['']
 info=''
-pre_encoded = {}
 predata = []
 
 def preload():
-    global pre_encoded, mode, mode_note, predata
+    global mode_note, predata
     with open('pre_encoded.txt', 'r') as f:
         pre_encoded = eval(f.read())
     for it in list(pre_encoded.keys()):
@@ -29,21 +26,29 @@ def preload():
     for jt in list(pre_encoded.values()):
         predata.append(jt)
 
+def oled_show():
+    global codein, info, mode, mode_note
+    oled.fill(0)
+    oled.DispChar(codein[-18:], 0, 0, 1)
+    oled.DispChar(info, 0, 16, 1)
+    oled.DispChar('MODE '+str(mode)+' '+mode_note[mode], 0, 48, 1)
+    oled.show()
+
 
 def on_touchpad_p_pressed(_):
-    global bpm, item, decoded, f_index, codein
-    codein = str(codein) + str('0')
+    global codein
+    codein += '0'
 
 touchpad_p.event_pressed = on_touchpad_p_pressed
 
 def on_touchpad_y_pressed(_):
-    global bpm, item, decoded, f_index, codein
-    codein = str(codein) + str('1')
+    global codein
+    codein += '1'
 
 touchpad_y.event_pressed = on_touchpad_y_pressed
 
 def on_touchpad_h_pressed(_):
-    global bpm, item, decoded, f_index, codein, times, ct, mode, mode_note
+    global codein, times, ct, info
     if time.time() - ct > 1.0:
         times = 0
     else:
@@ -53,75 +58,56 @@ def on_touchpad_h_pressed(_):
         codein = ''
         times = 0
         info = 'clear done'
-        oled.fill(0)
-        oled.DispChar(str(codein)[-18:], 0, 0, 1)
-        oled.DispChar(info, 0, 16, 1)
-        oled.DispChar('MODE '+str(mode)+mode_note[mode], 0, 48, 1)
-        oled.show()
+        oled_show()
         time.sleep(1)
         info = ''
 
 touchpad_h.event_pressed = on_touchpad_h_pressed
 
 def on_touchpad_o_pressed(_):
-    global bpm, item, decoded, f_index, codein, mode
-    mode = (mode+1)%2
+    global mode, mode_note
+    mode = (mode+1)%len(mode_note)
 
 touchpad_o.event_pressed = on_touchpad_o_pressed
 
 def on_touchpad_n_pressed(_):
-    global bpm, item, decoded, f_index, codein
+    global codein
     codein = codein[:-1]
 
 touchpad_n.event_pressed = on_touchpad_n_pressed
 
 
 def on_button_a_pressed(_):
-    global bpm, item, decoded, f_index, codein, pb, info, mode, mode_note
+    global decoded, codein, info
     decoded = decode(codein, 1)
     info = 'decoded successfully'
-    oled.fill(0)
-    oled.DispChar(str(codein)[-18:], 0, 0, 1)
-    oled.DispChar(info, 0, 16, 1)
-    oled.DispChar('MODE '+str(mode)+mode_note[mode], 0, 48, 1)
-    oled.show()
+    oled_show()
     time.sleep(1)
     info = ''
 
 button_a.event_pressed = on_button_a_pressed
 
 def on_button_b_pressed(_):
-    global bpm, item, decoded, f_index, codein, mode, mode_note, predata
+    global decoded, f_index, mode, predata, info, predata
     player = []
     if mode == 0:
-        player = decoded
+        player = decoded[:]
     else:
-        player = predata[mode-1]
-    bpm = player[-1]
-    pb = 15000/bpm
-    player.pop()
-    info = 'playing'
-    oled.fill(0)
-    oled.DispChar(str(codein)[-18:], 0, 0, 1)
-    oled.DispChar(info, 0, 16, 1)
-    oled.DispChar('MODE '+str(mode)+mode_note[mode], 0, 48, 1)
-    oled.show()
-    for item in player:
-        if item[0] <= 14:
+        player = predata[mode-1][:]
+    if player:
+        bpm = player.pop()
+        pb = 15000/bpm
+        info = 'playing'
+        oled_show()
+        for item in player:
             music.pitch(f_index[item[0]], int(pb*item[1]))
-        elif item[0] ==15:
-            music.pitch(0, int(pb*item[1]))
-    info = ''
+        info = ''
 
 button_b.event_pressed = on_button_b_pressed
 
 
 preload()
 while True:
-    oled.fill(0)
-    oled.DispChar(str(codein)[-18:], 0, 0, 1)
-    oled.DispChar(info, 0, 32, 1)
-    oled.DispChar('MODE '+str(mode)+mode_note[mode], 0, 48, 1)
-    oled.show()
+    oled_show()
 
 
